543e62cd6764b013de268cd449cf106e01aa9ff9
IIMare <jimengdi8724@sina.com>
2014-04-15 11:47:06 +0800

    Initial Commit

---
+++ AlarmClock/MDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MDAppDelegate.h
+//  AlarmClock
+//
+//  Created by IIMare on 4/15/14.
+//  Copyright (c) 2014 IIMare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlarmClock/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlarmClock
+//
+//  Created by IIMare on 4/15/14.
+//  Copyright (c) 2014 IIMare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MDAppDelegate class]));
+    }
+}
