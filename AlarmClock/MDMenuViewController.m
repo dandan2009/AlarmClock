023cde643b4c718d61f7689a774c3bc2e42264ee
IIMare <jimengdi8724@sina.com>
2014-05-28 01:16:16 +0800

    Commit 12

--- AlarmClock/MDMenuViewController.m
+++ AlarmClock/MDMenuViewController.m
@@ -8,7 +8,10 @@
 
 #import "MDMenuViewController.h"
 
-@interface MDMenuViewController ()
+@interface MDMenuViewController ()<UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UIImageView *menuImageView;
+@property (nonatomic, strong) UITableView *tableView;
 
 @end
 
@@ -18,7 +21,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        
     }
     return self;
 }
@@ -26,13 +29,79 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] bounds] style:UITableViewStyleGrouped];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    self.tableView.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:self.tableView];
+    
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - TableView Delegate and Datasource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (section == 0) {
+        
+        return 2;
+        
+    } else {
+        
+        return 1;
+        
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifer = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifer];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifer];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld",indexPath.row];
+    
+    
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 0) {
+        return 150;
+    } else {
+        return 0;
+    }
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *headView = [[UIView alloc] initWithFrame:CGRectMake(74,20, 130, 120)];
+    UIImageView *headImageView = [[UIImageView alloc] initWithFrame:CGRectMake(74, 20, 130, 120)];
+    headImageView.image = [UIImage imageNamed:@"clockImage.png"];
+    headImageView.contentMode = UIViewContentModeScaleAspectFit;
+    [headView addSubview:headImageView];
+    if (section == 0) {
+        return headView;
+    } else {
+        return nil;
+    }
+                                   
 }
 
 @end
